e3c658ee727b97a1878cfaaa2bcf5a9169870e3a
Matheus Dario-PC <dario.matheus97@gmail.com>
2021-04-10 10:16:52 -0300

    projeto em andamento

--- aula36.py
+++ aula36.py
@@ -1,5 +1,8 @@
 
-texto = 'Python'
+# texto = 'Python'
+#
+# for n, letra in enumerate(texto):
+#     print(n, letra)
 
-for n, letra in enumerate(texto):
-    print(n, letra)
+for numero in range(10):
+    print(numero)
